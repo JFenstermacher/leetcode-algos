de73cd3d66a3dd87057b1476c780d23cdc4903fd
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-09-19 13:24:56 -0400

    so simple... I feel dumb.

--- 55-JumpGame/main.go
+++ 55-JumpGame/main.go
@@ -1,6 +1,6 @@
 package main
 
-func canJump(nums []int) bool {
+func canJumpSlow(nums []int) bool {
 	last := len(nums) - 1
 	memo := map[int]bool{}
 
@@ -33,4 +33,22 @@ func canJump(nums []int) bool {
 	}
 
 	return recur(0)
+}
+
+func canJump(nums []int) bool {
+	max := 0
+
+	for i := 0; i < len(nums); i++ {
+		if i > max {
+			return false
+		} else {
+			currMax := i + nums[i]
+
+			if currMax > max {
+				max = currMax
+			}
+		}
+	}
+
+	return true
 }
