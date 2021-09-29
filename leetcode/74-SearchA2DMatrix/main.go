487b97698d2be001770d9b8ea4c3e03ed362f1e7
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2021-09-28 21:21:39 -0400

    stolen approach from top upload

--- leetcode/74-SearchA2DMatrix/main.go
+++ leetcode/74-SearchA2DMatrix/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func searchMatrix2(matrix [][]int, target int) bool {
+	row := sort.Search(len(matrix), func(i int) bool {
+		return matrix[i][0] > target
+	}) - 1
+
+	col := sort.SearchInts(matrix[row], target)
+
+	return col < len(matrix[row]) && matrix[row][col] == target
+}
 
 func searchMatrix(matrix [][]int, target int) bool {
 	r, c := 0, 0
